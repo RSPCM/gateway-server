38ac8e18044430a026294c427f57970df30e2499
endragon <asror0812@gmail.com>
2026-03-25 14:49:46 +0500

    fix

---
+++ src/main/java/com/example/gatewayservice/config/AsyncConfig.java
@@ -0,0 +1,24 @@
+package com.example.gatewayservice.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+
+import java.util.concurrent.Executor;
+
+@Configuration
+@EnableAsync
+public class AsyncConfig {
+
+    @Bean(name = "AuditLogTaskExecutor")
+    public Executor auditLogTaskExecutor() {
+        ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+        executor.setCorePoolSize(2);
+        executor.setMaxPoolSize(8);
+        executor.setQueueCapacity(500);
+        executor.setThreadNamePrefix("audit-log-");
+        executor.initialize();
+        return executor;
+    }
+}
